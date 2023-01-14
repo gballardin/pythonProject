bf5ce02bc57a02dfeca5aef0d1440a1662de824f
Giorgio Ballardin <giobal@amazon.com>
2023-01-14 15:48:32 -0800

    Second commit

---
+++ coin/coin.py
@@ -0,0 +1,44 @@
+from random import randint
+
+WIDTH=400
+HEIGHT=400
+score=0
+game_over=False
+
+fox=Actor("fox")
+fox.pos=100,100
+
+coin=Actor('coin')
+coin.pos=200,200
+
+def draw():
+    screen.fill("green")
+    fox.draw()
+    coin.draw()
+    screen.draw.text(f"Score {str(score)}", color='black', topleft=(10,10))
+    if game_over:
+        screen.fill('pink')
+        screen.draw.text(f"Score {str(score)}", topleft=(10, 10), fontsize=60)
+
+def place_coin():
+    coin.x=randint(20, (WIDTH-20))
+    coin.y = randint(20, (HEIGHT - 20))
+
+def time_up():
+    global game_over
+    game_over=True
+
+def update():
+    global score
+    if keyboard.left:
+        fox.x -= 2
+    elif keyboard.right:
+        fox.x += 2
+    elif keyboard.up:
+        fox.y -= 2
+    elif keyboard.down:
+        fox.y += 2
+    coin_collected=fox.colliderect(coin)
+    if coin_collected:
+        score +=10
+        place_coin()
